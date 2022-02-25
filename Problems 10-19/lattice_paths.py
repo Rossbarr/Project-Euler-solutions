c66dbdfc30d6773c689474cbde7069d5240a8dd9
rossbarr <barrettross23@gmail.com>
2022-02-24 20:04:05 -0500

    work on lattice paths, need to use recursion

--- Problems 10-19/lattice_paths.py
+++ Problems 10-19/lattice_paths.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 right = "r"
 down = "d"
 
@@ -7,13 +10,31 @@ two = [[right, right, down, down], [right, down, right, down],
        [right, down, down, right], [down, right, right, down], 
        [down, right, down, right], [down, down, right, right]]
 
-def lattice_paths(n: int):
+def lattice_paths(side_length: int, paths: list = [], n: int = 0) -> list:
     """
     Computes number of possible equal-length paths through an
     n by n lattice.
     """
-    paths = []
-    while True:
-        direction = right
-        path = []
-        path << direction
+
+    if (n == 0):
+        paths = [[right], [down]];
+    
+    for path in paths:
+        if can_add_more_path(path, side_length, right):
+            paths.append(path + [right])
+        if can_add_more_path(path, side_length, down):
+            paths.append(path + [down])
+
+
+    return [paths]
+
+def can_add_more_path(path: list, side_length: int, dir: str) -> bool:
+    count = 0
+    for ele in list:
+        if ele == dir:
+            count += 1
+    
+    if count >= side_length:
+        return True
+    else:
+        return False
